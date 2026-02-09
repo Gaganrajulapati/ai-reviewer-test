9b490cbfee353e52d1549363d3e2f9d632e0e66f
Gagankumar.Rajulapati <99827270+Gaganrajulapati@users.noreply.github.com>
2026-02-10 00:09:13 +0530

    Implement login function with security risks
    Added a login function with hardcoded password and SQL injection risk.

---
+++ bad_code.py
@@ -0,0 +1,10 @@
+import os[[3](https://www.google.com/url?sa=E&q=https%3A%2F%2Fvertexaisearch.cloud.google.com%2Fgrounding-api-redirect%2FAUZIYQEkJktvpUouLhA7uFAifTRn-ZhjO6IVdxmedsT42hcALWYIpvnEcSmRKVMzC-zOdPc7IdW5gKFmMzerKxS8yLGJw1yqwcbfOLazSzLmMLAgSaNlIlOAVvuYbX5qCdPo0sZF6xRHo0k%3D)]
+
+def login(user, password):
+    # Hardcoded password - Security Risk!
+    if password == "12345":
+        print("Access Granted")[[3](https://www.google.com/url?sa=E&q=https%3A%2F%2Fvertexaisearch.cloud.google.com%2Fgrounding-api-redirect%2FAUZIYQEkJktvpUouLhA7uFAifTRn-ZhjO6IVdxmedsT42hcALWYIpvnEcSmRKVMzC-zOdPc7IdW5gKFmMzerKxS8yLGJw1yqwcbfOLazSzLmMLAgSaNlIlOAVvuYbX5qCdPo0sZF6xRHo0k%3D)]
+    
+    # SQL Injection Risk![[3](https://www.google.com/url?sa=E&q=https%3A%2F%2Fvertexaisearch.cloud.google.com%2Fgrounding-api-redirect%2FAUZIYQEkJktvpUouLhA7uFAifTRn-ZhjO6IVdxmedsT42hcALWYIpvnEcSmRKVMzC-zOdPc7IdW5gKFmMzerKxS8yLGJw1yqwcbfOLazSzLmMLAgSaNlIlOAVvuYbX5qCdPo0sZF6xRHo0k%3D)]
+    query = f"SELECT * FROM users WHERE name = {user}"
+    print(query)
